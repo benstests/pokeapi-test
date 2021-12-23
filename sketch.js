81fea8dec9ca74bead408aae4e65635c410b4479
benstests <95652162+benstests@users.noreply.github.com>
2021-12-23 15:07:20 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,6 @@ const pokeData = {
   url: 'https://pokeapi.co/api/v2/pokemon/',
   id: '1',
 }
-const apiUrl = document.getElementById(pokeData.url) + '/' + document.getElementById(pokeData.id);
+const apiUrl = document.getElementById(pokeData.url) + document.getElementById(pokeData.id);
 
 console.log(apiUrl);
