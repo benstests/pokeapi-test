67f9bd4d08122437c831ecdb4c41f929ac178072
benstests <95652162+benstests@users.noreply.github.com>
2021-12-23 15:00:51 +0000

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,7 @@
+const pokeData = {
+  url: 'https://pokeapi.co/api/v2/pokemon/',
+  id: '1',
+}
+const apiUrl = document.getElementById(pokeData.url) + '/' + document.getElementById(pokeData.id);
+
+console.log(apiUrl);
